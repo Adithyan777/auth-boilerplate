7cf129a0e871cb6a4d006d8ab1129bd6109a65b0
Adithyan K <adithyn.krshna@gmail.com>
2024-07-25 15:15:26 +0530

    small changes

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import { validateRequest } from "@/lib/validate-requests";
 import { logout } from "./signout_action";
+import Link from "next/link";
 
 export default async function Home() {
 
   const {user} = await validateRequest();
-  console.log(user);
 
   return (
     <>
@@ -13,10 +13,16 @@ export default async function Home() {
     <div className="m-4"> 
     Welcome, {user.firstName} {user.lastName}
     <form action={logout}>
-			<button>Sign out</button>
+			<button className="underline">Sign out</button>
 		</form>
     </div>}
-    {!user && <div className="m-4"> Please sign in </div>}
+    {!user && 
+    <>
+    <div className="m-4">
+       Please sign in 
+    </div>
+       <Link href="/login" className="underline m-4">Login</Link>
+    </>}
     </>
   );
 }
